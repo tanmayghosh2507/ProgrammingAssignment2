eec2cc68172d862f9ab2406dc521ba6811c97c0a
Tanmay Ghosh <tnmyiitr@gmail.com>
2016-09-06 15:01:33 +0530

    Cached Matrix Inverse Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Equivalent to POJO with setter and getter in JAVA.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inv) m <<- inv
+        getInverse <- function() m
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function checks whether the inverse of the matrix already exists, if so, 
+## it just prints the cached result otherwise it delegates the flow to makeCachedMatrix 
+## function and calculates it's inverse using solve(X).
 
 cacheSolve <- function(x, ...) {
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                print("Printing cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data,...)
+        x$setInverse(m)
+        m
         ## Return a matrix that is the inverse of 'x'
 }
